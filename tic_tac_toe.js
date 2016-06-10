8b8a36ec05487a9eaebbcb2b3e3f078cc07488ba
garity <garitychapman@gmail.com>
2016-06-10 15:03:01 -0400

    Added Ascii art for Welcome and Win/Lose Msgs

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,6 +1,8 @@
 // TO DO
 // - P2Move AI
 // - refactor to use stdin instead of prompt
+// - want to play again?
+// - pick p1 or p2/ computer vs computer etc.
 
 var prompt = require('prompt');
 
@@ -50,7 +52,6 @@ function askInput(callback){
     if (err){ 
       throw err;
     }else{
-      console.log("input is: " + result.input);
       //check for valid input
       if(getCellByInput(result.input) == EMPTY){
         callback(result.input);
@@ -97,7 +98,7 @@ function updateBoard(board, input){
   return board;
 }
 
-//helper function to check for all cells matching
+//checks for matching cells
 function cellsMatch(arr, val) {
   var testArr = [];
   for (var i = 0; i < arr.length; i++) {
@@ -188,20 +189,51 @@ function gameWon(board){
     return true;
   }
   if(p1Win){
-    console.log("Game over. Player one, you win!");
+    p1WinnerMsg();
   }else if(p2Win){
-    console.log("Game over. Player two, you win!");
+    p1LoseMsg();
   }
   return p1Win || p2Win;
 }
+
+function welcomeMsg(){
+  var welcome = [["+-+-+-+-+-+-+-+ +-+-+ +-+-+-+ +-+-+-+ +-+-+-+-+"],
+              ["|W|e|l|c|o|m|e| |t|o| |T|i|c| |T|a|c| |T|o|e|!|"],
+              ["+-+-+-+-+-+-+-+ +-+-+ +-+-+-+ +-+-+-+ +-+-+-+-+"]];
+  console.log(welcome.join("\n"));
+}
+
+function p1WinnerMsg(){
+  var youWin = [
+    ["  __   __   U  ___ u   _   _                             _   _     _   "],
+    ["  \\ \\ / /    \\/\"_ \\/U |\"|u| |  __        __     ___     | \\ |\"|  U|\"|u "],
+    ["   \\ V /     | | | | \\| |\\| |  \\\"\\      /\"/    |_\"_|   <|  \\| |> \\| |/ "],
+    ["  U_|\"|_u.-,_| |_| |  | |_| |  /\\ \\ /\\ / /\\     | |    U| |\\  |u  |_|  "],
+    ["    |_|   \\_)-\\___/  <<\\___/  U  \\ V  V /  U  U/| |\\u   |_| \\_|   (_)  "],
+    [".-,//|(_       \\\\   (__) )(   .-,_\\ /\\ /_,-.-,_|___|_,-.||   \\\\,-.|||_ "],
+    [" \\_) (__)     (__)      (__)   \\_)-'  '-(_/ \\_)-' '-(_/ (_\")  (_/(__)_)"]];
+  console.log(youWin.join("\n"));
+}
+
+function p1LoseMsg(){
+var youLoseMsg = [["  _,  _, __, __, , _     , _  _, _,_   _,   _,  _, __, "],
+                  [" (_  / \\ |_) |_) \\ |     \\ | / \\ | |   |   / \\ (_  |_  "],
+                  [" , ) \\ / | \\ | \\  \\| ,    \\| \\ / | |   | , \\ / , ) |  ,"],
+                  ["  ~   ~  ~ ~ ~ ~   ) '     )  ~  `~'   ~~~  ~   ~  ~~~~"],
+                  ["                  ~'      ~'                           "]];
+  console.log(youLoseMsg.join("\n"));
+
+}
+
 
 
 //turn function to run game
 function turn(board){ 
   //starts the game: draws the board & gives instructions
   if(turnCount === 0){
+    welcomeMsg();
+    console.log("You are Player One.\nThe spaces on the board are numbered 1 through 9.\nGood luck!\n");
     drawBoard(board);
-    console.log("Welcome to Tic Tac Toe! You are Player 1. The spaces on the board are numbered 1 through 9. Good luck!");
   }
   //P1 plays on even turns. Asks P1 for input, redraws board and increments turn counter 
   if(turnCount % 2 === 0){
@@ -209,7 +241,6 @@ function turn(board){
       console.log("Player one's turn.");
       drawBoard(updateBoard(board, input));
       turnCount += 1;
-      console.log("turn #" + turnCount);
       //if the game is not won call turn
       if(!gameWon(board)){
         turn(board);
@@ -219,14 +250,19 @@ function turn(board){
   //P2 plays on odd turns. Generates move from P2, redraws board and increments turn counter
   else if(turnCount % 2 != 0){
     //TODO: add in delay between players turns
-    console.log("PLayer two's turn.");
-    drawBoard(updateBoard(board, player2Move(board)));
-    turnCount += 1;
-    console.log("turn #" + turnCount);
+    console.log("Player two's turn.");
+    setTimeout(function(){
+      console.log("Thinking...")
+    }, 500);
+    setTimeout(function(){
+      drawBoard(updateBoard(board, player2Move(board)));
+      turnCount += 1;
     //if the game is not won call turn
     if(!gameWon(board)){
         turn(board);
       }
+    }, 2000);
+    
   }
 };
 //first call to turn
